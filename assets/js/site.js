3d65218c4508ff240701d18c3154c11c59ba6e4c
Daniel Kastl <daniel@georepublic.de>
2019-10-21 01:01:11 +0900

    Menu update to work better on mobile

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,20 +1,20 @@
 import $ from 'jquery';
 import 'bootstrap';
 
-function toggleDropdown (e) {
-  const _d = $(e.target).closest('.dropdown'),
-    _m = $('.dropdown-menu', _d);
-  setTimeout(function(){
-    const shouldOpen = e.type !== 'click' && _d.is(':hover');
-    _m.toggleClass('show', shouldOpen);
-    _d.toggleClass('show', shouldOpen);
-    $('[data-toggle="dropdown"]', _d).attr('aria-expanded', shouldOpen);
-  }, e.type === 'mouseleave' ? 100 : 0);
-}
+// function toggleDropdown (e) {
+//   const _d = $(e.target).closest('.dropdown'),
+//     _m = $('.dropdown-menu', _d);
+//   setTimeout(function(){
+//     const shouldOpen = e.type !== 'click' && _d.is(':hover');
+//     _m.toggleClass('show', shouldOpen);
+//     _d.toggleClass('show', shouldOpen);
+//     $('[data-toggle="dropdown"]', _d).attr('aria-expanded', shouldOpen);
+//   }, e.type === 'mouseleave' ? 100 : 0);
+// }
 
-$('body')
-  .on('mouseenter mouseleave touchstart','.dropdown',toggleDropdown)
-  .on('click', '.dropdown-menu a', toggleDropdown);
+// $('body')
+//   .on('mouseenter mouseleave touchstart','.dropdown',toggleDropdown)
+//   .on('click', '.dropdown-menu a', toggleDropdown);
 
 window.cookieconsent.initialise({
   "palette": {
