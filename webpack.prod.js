8a315b20fc405ee09d5d291cce50d9a428d492a9
Daniel Kastl <daniel@georepublic.de>
2020-04-15 15:38:31 +0900

    Temporarly do not uglify

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,8 +2,12 @@ const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
 
+// module.exports = merge(common, {
+//   plugins: [
+//     new UglifyJSPlugin()
+//   ]
+// });
+
 module.exports = merge(common, {
-  plugins: [
-    new UglifyJSPlugin()
-  ]
+  plugins: []
 });
